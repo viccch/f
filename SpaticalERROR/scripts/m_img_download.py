85a9881258acd397fc2e3c8266709ebb5c8d3bd1
viccch <zhangshijie2002@gmail.com>
2022-11-08 22:21:54 +0800

    人文地理学

--- SpaticalERROR/scripts/m_img_download.py
+++ SpaticalERROR/scripts/m_img_download.py
@@ -5,9 +5,9 @@ from urllib import request
 # r=request.urlretrieve(url,"1.png")
 
 
-n_from = 0  # 起始页码
-n_to = 43  # 结束页码
-base_url = "https://s3.ananas.chaoxing.com/sv-w9/doc/d6/a8/d1/52212c46a9ee278d13c8f2e1850bb8ce/thumb/"
+n_from = 1  # 起始页码
+n_to = 36  # 结束页码
+base_url = "https://s3.ananas.chaoxing.com/doc/b1/f6/64/2eab9ef88373f37e5ca1e8e145c94244/thumb/"
 
 for i in range(n_from, n_to+1, 1):
     img_name = str(i)+".png"
