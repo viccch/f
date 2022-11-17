3fbe3f9ed4266af85ecf9f21a26ba767787ed44c
viccch <zhangshijie2002@gmail.com>
2022-11-17 23:26:31 +0800

    s

--- java/VariablePara.java
+++
@@ -1,9 +0,0 @@
-public class Varia {
-    public static void main(String args[]) {
-
-    }
-
-static void  change(m obj){
-    
-}
-}
--- java/m.java
+++
@@ -1,9 +0,0 @@
-public class m {
-    public static void main(String args[]) {
-
-    }
-
-static void  change(m obj){
-    
-}
-}
